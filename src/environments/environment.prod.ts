89fb642ca39008ef681b373178ec4c950617610b
sweta suman <swetasuman295@gmail.com>
2025-09-06 15:38:37 +0200

    ws change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
     apiUrl: 'https://api.familyhomecloud.synology.me/api',
-    wsUrl: 'wss://api.familyhomecloud.synology.me/api/ws'
+    wsUrl: 'https://api.familyhomecloud.synology.me/api/ws'
 };
