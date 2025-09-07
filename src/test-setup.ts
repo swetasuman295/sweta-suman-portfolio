8866678a0b524c174921a169a4a3fa50aed59ab4
sweta suman <swetasuman295@gmail.com>
2025-09-07 22:34:29 +0200

    added build fix for failing polyfills

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,2 +1,13 @@
 // Fix for SockJS in test environment
 (window as any).global = window;
+(window as any).process = { env: {} };
+
+// Additional SockJS fixes
+Object.defineProperty(window, 'WebSocket', {
+  writable: true,
+  value: class MockWebSocket {
+    constructor() {}
+    send() {}
+    close() {}
+  }
+});
